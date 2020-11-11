b57039cf8d1ae490947187072c17316985b1a148
Maurits De Muer <maurits.demuer@student.ap.be>
2020-11-11 22:29:29 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
-import { Wrapper, Image, BottomEdgeDown, BottomEdgeUp, Artist } from "./pageStyles/pageStyles"
+import {
+  Wrapper,
+  Image,
+  BottomEdgeDown,
+  BottomEdgeUp,
+  Artist,
+} from "./pageStyles/pageStyles"
 import { COLORS } from "../constants"
 
 const IndexPage = () => {
@@ -12,93 +19,98 @@ const IndexPage = () => {
         homeMeta: {
           homePageDescription,
           homePageFeaturedArtists,
+          homePageHeaderDescription,
           homePageHeaderPicture,
           homePageHeaderTitle,
-      }
-    }
-  }
-} = useStaticQuery(graphql`
-query {
-  wpcontent {
-	page(id: "home", idType: URI) {
-		homeMeta {
-			homePageDescription
-			homePageHeaderDescription
-			homePageHeaderTitle
-			homePageHeaderPicture {
-        altText
-        sourceUrl
-        imageFile {
-          childImageSharp {
-            fluid(quality: 100) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-			}
-			homePageFeaturedArtists {
-				... on WPGraphql_Artist {
-					id
-					artist {
-						artistName
-						firstName
-						lastName
-						profile {
+        },
+      },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      wpcontent {
+        page(id: "home", idType: URI) {
+          homeMeta {
+            homePageDescription
+            homePageHeaderDescription
+            homePageHeaderTitle
+            homePageHeaderPicture {
               altText
               sourceUrl
               imageFile {
                 childImageSharp {
-                  fluid(quality: 100, grayscale: true) {
+                  fluid(quality: 100) {
                     ...GatsbyImageSharpFluid_withWebp
                   }
                 }
               }
-						}
-					}
-				}
-			}
-		}
-	}
-}
-}
-`);
+            }
+            homePageFeaturedArtists {
+              ... on WPGraphql_Artist {
+                slug
+                artist {
+                  artistName
+                  firstName
+                  lastName
+                  profile {
+                    altText
+                    sourceUrl
+                    imageFile {
+                      childImageSharp {
+                        fluid(quality: 100, grayscale: true) {
+                          ...GatsbyImageSharpFluid_withWebp
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
-  <Layout>
-    <SEO title="Home" />
-    <Wrapper>
-      <div className = "banner">
-        <Image fluid={homePageHeaderPicture.imageFile.childImageSharp.fluid} 
-        alt={homePageHeaderPicture.altText}
-        />
-      <div className="inner-div">
-        <p className="header-title">{homePageHeaderTitle}</p>
-        <p className="header-description">{homePageDescription}</p>
-      </div>
-      <BottomEdgeDown color={COLORS.BLACK}/>
-      </div>
-      <div className="description">
-        <p>{homePageDescription}</p>
-        <BottomEdgeUp color={COLORS.PRIMARY}/>
+    <Layout>
+      <SEO title="Home" />
+      <Wrapper>
+        <div className="banner">
+          <Image
+            fluid={homePageHeaderPicture.imageFile.childImageSharp.fluid}
+            alt={homePageHeaderPicture.altText}
+          />
+          <div className="inner-div">
+            <p className="header-title">{homePageHeaderTitle}</p>
+            <p className="header-description">{homePageHeaderDescription}</p>
+          </div>
+          <BottomEdgeDown color={COLORS.BLACK} />
+        </div>
+        <div className="description">
+          <p>{homePageDescription}</p>
+          <BottomEdgeUp color={COLORS.PRIMARY} />
         </div>
         <div className="artists">
           <h2>Featured Artists</h2>
           <div className="artist-items">
-            {homePageFeaturedArtists.map(({artist, slug}) => (
-              <Artist to={`${slug}`}>
+            {homePageFeaturedArtists.map(({ artist, slug }) => (
+              <Artist to={`/${slug}`}>
                 <Image
-                 fluid={artist.profile.imageFile.childImageSharp} 
-                 altText={artist.profile.altText} 
-                 />
+                  fluid={artist.profile.imageFile.childImageSharp.fluid}
+                  alt={artist.profile.altText}
+                />
                 <div className="artist-info">
-                  <p>{artist.firstName} {artist.lastName}</p>
+                  <p>
+                    {artist.firstName} {artist.lastName}
+                  </p>
                   <p>{artist.artistName}</p>
                 </div>
               </Artist>
             ))}
           </div>
         </div>
-    </Wrapper>
-  </Layout>
+      </Wrapper>
+    </Layout>
   )
 }
 
